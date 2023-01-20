800466baecb23b4f8e565ff228f1262647437e8f
abdelazizrashed <abdelaziz.y.rashed@gmail.com>
2023-01-20 21:56:39 +0200

    Setup project structure and Add some dependencies

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:evaluation_project/ui/screens/home/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
